d80a08c83cd7df6a2d3d512d185b20c242a0e524
PurpleRabbit75 <abrahambaker104@gmail.com>
2026-02-15 23:35:14 -0600

    Added code to append one schedule entry. This is located in a custom container

--- generator.py
+++ generator.py
@@ -2,11 +2,30 @@ import streamlit as st
 import json
 from io import StringIO
 
-
+days_mapping = {"Monday" : "M", "Tuesday" : "T", "Wednesday" : "W", "Thursday" : "R", "Friday" : "F"}
 data = []
 
 name = st.text_input("Name:")
 data.append(name)
+
+st.divider()
+
+with st.container():
+    entry = []
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        start_time = st.time_input("Start Time:", value = None)
+        if start_time is not None:
+            entry.append([start_time.hour, start_time.minute])
+    with col2:
+        end_time = st.time_input("End Time:", value = None)
+        if end_time is not None:
+            entry.append([end_time.hour, end_time.minute])
+    with col3:
+        days = st.multiselect("Days of the Week:", ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"])
+        entry.append(''.join([days_mapping[day] for day in days]))
+    data.append(entry)
+    
 
 
 
